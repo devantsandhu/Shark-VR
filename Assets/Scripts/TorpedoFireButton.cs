7a51d3afc9565456b90e98f9753643a74e025077
Devant Sandhu <devantsandhu@gmail.com>
2021-05-06 16:28:14 -0700

    adding prev proj

---
+++ Assets/Scripts/TorpedoFireButton.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+
+public class TorpedoFireButton : MonoBehaviour, IPointerDownHandler {
+
+	public SubmarineController subController;
+
+	public void OnPointerDown (UnityEngine.EventSystems.PointerEventData pointerData) {
+
+		subController.FireTorpedo ();
+
+	}
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# Shark-VR
+
+A VR android app built in Unity-3D. User is in a submarine underwater and must survive the sharks.
